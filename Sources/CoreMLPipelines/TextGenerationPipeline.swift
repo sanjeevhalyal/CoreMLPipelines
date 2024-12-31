4dccda31dd108649eb969542cb7d7edd976132cd
Finn Voorhees <finnvoorhees@gmail.com>
2024-12-31 16:39:42 -0500

    Add SmolLM2

--- Sources/CoreMLPipelines/TextGenerationPipeline.swift
+++ Sources/CoreMLPipelines/TextGenerationPipeline.swift
@@ -252,5 +252,14 @@ public extension TextGenerationPipeline {
         case qwen2_5_3B_Instruct_4bit = "finnvoorhees/coreml-Qwen2.5-3B-Instruct-4bit"
         case qwen2_5_Coder_0_5B_Instruct_4bit = "finnvoorhees/coreml-Qwen2.5-Coder-0.5B-Instruct-4bit"
         case qwen2_5_Coder_1_5B_Instruct_4bit = "finnvoorhees/coreml-Qwen2.5-Coder-1.5B-Instruct-4bit"
+
+        // MARK: - SmolLM2
+
+        case smolLM2_135M_Instruct_4bit = "finnvoorhees/coreml-SmolLM2-135M-Instruct-4bit"
+        case smolLM2_135M_Instruct_8bit = "finnvoorhees/coreml-SmolLM2-135M-Instruct-8bit"
+        case smolLM2_1_7B_Instruct_4bit = "finnvoorhees/coreml-SmolLM2-1.7B-Instruct-4bit"
+        case smolLM2_360M_Instruct_4bit = "finnvoorhees/coreml-SmolLM2-360M-Instruct-4bit"
+        case smolLM2_360M_Instruct_8bit = "finnvoorhees/coreml-SmolLM2-360M-Instruct-8bit"
+        case smolLM2_1_7B_Instruct_8bit = "finnvoorhees/coreml-SmolLM2-1.7B-Instruct-8bit"
     }
 }
