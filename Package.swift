5eaf4d3561a8ad0500e5d666c29e02192a68f9bd
sanjeev Halyal <1448731+sanjeevhalyal@users.noreply.github.com>
2025-06-01 00:01:57 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/huggingface/swift-transformers.git", branch: "main"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
         .package(url: "https://github.com/finnvoor/SwiftTUI.git", branch: "main")
     ],
     targets: [
